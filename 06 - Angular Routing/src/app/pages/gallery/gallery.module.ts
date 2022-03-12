8cebe69170a5c2ada1600588b0577fefad7f02e2
Zoltan R. Janki <jankiz@inf.u-szeged.hu>
2022-03-12 11:41:45 +0100

    6th practice added.

---
+++ 06 - Angular Routing/src/app/pages/gallery/gallery.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GalleryRoutingModule } from './gallery-routing.module';
+import { GalleryComponent } from './gallery.component';
+import { ListComponent } from './list/list.component';
+import { ViewerComponent } from './viewer/viewer.component';
+import { FormsModule } from '@angular/forms';
+import { DateFormatPipe } from '../../shared/pipes/date-format.pipe';
+
+
+@NgModule({
+  declarations: [
+    GalleryComponent,
+    ListComponent,
+    ViewerComponent,
+    DateFormatPipe
+  ],
+  imports: [
+    CommonModule,
+    GalleryRoutingModule,
+    FormsModule
+  ]
+})
+export class GalleryModule { }

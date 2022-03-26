28a0581e20558721cf1cf90b62a14c543db27671
Zoltan R. Janki <jankiz@inf.u-szeged.hu>
2022-03-26 14:35:19 +0100

    8th practice material added.

---
+++ 08 - Angular Services/src/app/shared/pipes/date-format.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateFormat'
+})
+export class DateFormatPipe implements PipeTransform {
+
+  transform(value: Date, ...args: unknown[]): string {
+    let tzoffset = (new Date(value)).getTimezoneOffset() * 60000
+    let localISOTime = (new Date(Date.now() - tzoffset)).toISOString().replace('Z', '').replace('T', ' ');
+    return localISOTime
+    // return null;
+  }
+
+}

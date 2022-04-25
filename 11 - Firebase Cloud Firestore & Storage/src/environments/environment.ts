b3242f87aae6a339b479b471e2ef047d0b84b18d
Zoltan R. Janki <jankiz@inf.u-szeged.hu>
2022-04-25 18:57:17 +0200

    envs removed.

--- 11 - Firebase Cloud Firestore & Storage/src/environments/environment.ts
+++ 11 - Firebase Cloud Firestore & Storage/src/environments/environment.ts
@@ -4,13 +4,7 @@
 
 export const environment = {
   firebase: {
-    projectId: 'webfejl-2022',
-    appId: '1:309741695067:web:ad2288a932e6e39310cf7e',
-    storageBucket: 'webfejl-2022.appspot.com',
-    apiKey: 'AIzaSyARi2Gsx11rTcwEVUBJclhruF2gynW1uhY',
-    authDomain: 'webfejl-2022.firebaseapp.com',
-    messagingSenderId: '309741695067',
-    measurementId: 'G-N2FNTN5NE3',
+    /* TODO: put your environment here */
   },
   production: false,
   hostUrl: 'http://localhost:4200'
